610f762c670819a2d1f27c252bc87bcdc30be1de
zeitunamar <zeitunamar@gmail.com>
2025-08-06 22:38:15 +0300

    Add Create Main Activity button to Planning.tsx

--- src/pages/Planning.tsx
+++ src/pages/Planning.tsx
@@ -792,18 +792,33 @@ const Planning: React.FC = () => {
                     Main Activities
                   </h3>
                   {selectedInitiative ? (
-                    <MainActivityList
-                      initiativeId={selectedInitiative.id}
-                      initiativeWeight={Number(selectedInitiative.weight)}
-                      onEditActivity={handleEditActivity}
-                      onSelectActivity={() => {}}
-                      onAddBudget={handleAddBudget}
-                      onEditBudget={handleEditBudget}
-                      onViewBudget={handleViewBudget}
-                      planKey={`planning-${refreshKey}`}
-                      isUserPlanner={isUserPlanner}
-                      userOrgId={userOrgId}
-                    />
+                    <div className="space-y-4">
+                      <MainActivityList
+                        initiativeId={selectedInitiative.id}
+                        initiativeWeight={Number(selectedInitiative.weight)}
+                        onEditActivity={handleEditActivity}
+                        onSelectActivity={() => {}}
+                        onAddBudget={handleAddBudget}
+                        onEditBudget={handleEditBudget}
+                        onViewBudget={handleViewBudget}
+                        planKey={`planning-${refreshKey}`}
+                        isUserPlanner={isUserPlanner}
+                        userOrgId={userOrgId}
+                      />
+                      
+                      {/* Create Main Activity Button */}
+                      {isUserPlanner && (
+                        <div className="text-center pt-4">
+                          <button 
+                            onClick={() => handleEditActivity({})}
+                            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+                          >
+                            <Plus className="h-4 w-4 mr-2" />
+                            Create Main Activity
+                          </button>
+                        </div>
+                      )}
+                    </div>
                   ) : (
                     <div className="text-center p-6 bg-gray-50 rounded-lg border-2 border-dashed border-gray-200">
                       <DollarSign className="h-6 w-6 text-gray-400 mx-auto mb-2" />
